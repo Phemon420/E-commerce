8eab93aae610abd35577fbf732419cc0e8575a14
Phemon420 <113652475+Phemon420@users.noreply.github.com>
2024-12-15 12:28:50 +0530

    Update home.jsx

--- my-react-app/src/components/home/home.jsx
+++ my-react-app/src/components/home/home.jsx
@@ -1,40 +1,52 @@
-import NavBar from './Navbar'
-import Banner from './Banner'
-import { Fragment } from 'react';
-import {AppBar,Toolbar,styled,Box,Typography, Button} from '@mui/material';
-import { useEffect } from 'react';
-import { getProducts } from '../../redux/actions/productactions';
+import React, { Fragment, useEffect } from 'react';
+import { styled, Box } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../redux/actions/productactions';
+
+import NavBar from './Navbar';
+import Banner from './Banner';
 import MidSlide from './midslider';
 import Midsection from './midsection';
 
-
-const Padder=styled(Box)`
-    padding:10px;
+// Styled Components
+const Padder = styled(Box)`
+    padding: 10px;
     background: #303030;
-`
+    max-width: 1200px;
+    margin: 0 auto; /* Center the content */
+    overflow-x: hidden; /* Prevent horizontal scrolling */
+`;
 
+const Layout = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 110vw;
+    overflow-x: hidden;
+    padding:10px;4
+`;
 
-function Home(){
-    const {products}=useSelector(state=>state.getProducts);
-    const dispatch=useDispatch();
-    useEffect(()=>{
-        dispatch(getProducts())
-    },[dispatch])
+function Home() {
+    const { products } = useSelector((state) => state.getProducts);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch]);
 
     return (
-        <Fragment>
-            <NavBar/>
+        <Layout>
+            <NavBar />
             <Padder>
-                <Banner/>
-                <MidSlide products={products} oneliner="deals of the day" timer={true}/>
-                <Midsection></Midsection>
-                <MidSlide products={products} oneliner="deals you can not miss" timer={false}/>
-                <MidSlide products={products} oneliner="lowest price ever" timer={false}/>
-                <MidSlide products={products} oneliner="lund lelo" timer={true}/>
+                <Banner />
+                <MidSlide products={products} oneliner="Deals of the day" timer={true} />
+                <Midsection />
+                <MidSlide products={products} oneliner="Deals you cannot miss" timer={false} />
+                <MidSlide products={products} oneliner="Lowest price ever" timer={false} />
+                <MidSlide products={products} oneliner="Exciting offers" timer={true} />
             </Padder>
-        </Fragment>
-    )
+        </Layout>
+    );
 }
 
 export default Home;
